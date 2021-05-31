663b2e6f0f1494d66ee9c71d018f3b7f327df865
alasali1 <alasali96@gmail.com>
2021-05-31 15:17:24 -0700

    assignment 3 changes

--- app/src/androidTest/java/com/example/form/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/form/SignUpActivityTest.java
@@ -8,6 +8,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
@@ -15,5 +20,20 @@ public class SignUpActivityTest {
     @Rule
     public ActivityScenarioRule<SignUpActivity> activityScenarioRule
             = new ActivityScenarioRule<>(SignUpActivity.class);
+
+    @Test
+    public void retainsStateAfterRotate() {
+        // Change state of buttons / textview
+        onView(withId(R.id.name)).perform(typeText("Bill"));
+
+        // Ensure change happened
+        onView(withId(R.id.name)).check(matches(withText("Bill")));
+
+        TestUtils.rotateScreen(TestUtils.getActivity(activityScenarioRule));
+
+        // Ensure change is still there
+        onView(withId(R.id.name)).check(matches(withText("Bill")));
+    }
+
 
 }

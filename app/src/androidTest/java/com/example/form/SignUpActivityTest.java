b6d8f5f29c00a6c84c3544dcb8f02dbfb2b60901
alasali1 <alasali96@gmail.com>
2021-05-30 22:14:07 -0700

    assignment 3 changes

---
+++ app/src/androidTest/java/com/example/form/SignUpActivityTest.java
@@ -0,0 +1,19 @@
+package com.example.form;
+
+
+import org.junit.Rule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+@RunWith(JUnit4.class)
+public class SignUpActivityTest {
+    @Rule
+    public ActivityScenarioRule<SignUpActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(SignUpActivity.class);
+
+}
